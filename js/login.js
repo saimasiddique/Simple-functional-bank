2b01d7503907c7c8216109eed1e5e584cc5688e1
saimasiddique <siddiquesaima16022@gmail.com>
2022-02-12 16:03:14 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,22 @@
+document.getElementById('login-submit').addEventListener('click', function () {
+
+    // console.log('button clicked');
+    //get user email
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    // console.log(userEmail);
+
+    //get user password
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+
+    //get email && password
+    if (userEmail == 'sontan@baap.com' && userPassword == 'secret') {
+        // console.log('valid user');
+
+        window.location.href = 'banking.html';
+    }
+
+
+});
+
